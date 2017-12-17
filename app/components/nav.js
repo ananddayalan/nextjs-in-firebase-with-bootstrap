0aadf456aaba97486a8b0ae51e2b90c0b6d85a41
Aanand Dayalan <anand.dayalan@gmail.com>
2017-12-17 11:35:49 -0800

    fixing refs

--- app/components/nav.js
+++ app/components/nav.js
@@ -3,7 +3,7 @@ import Head from './head'
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-dark fixed-top navbar-shrink" id="mainNav">
        <div className="container">
-         <a className="navbar-brand js-scroll-trigger" href="#top">NextJS in Firebase with Bootstrap</a>
+         <a className="navbar-brand js-scroll-trigger" href="/#top">NextJS in Firebase with Bootstrap</a>
          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
            Menu
            <i className="fa fa-bars"></i>
